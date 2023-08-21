645f5843cf884eeaf76414da7ccb9628f105dce3
AliKandiAK <ali.jarsomah12@gmail.com>
2023-08-21 16:21:54 +0300

    updated

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 d = {}
 with open('Data.txt') as file:
     for line in file:
@@ -6,30 +5,31 @@ with open('Data.txt') as file:
         key = parts[0]
         val = ",".join(parts[1:])  # Join the remaining parts to get the value
         d[key] = val
-
-print(d)
-def genders(d):#1
+#users functions
+def find_user(user, d):#O(n*m) m is the avarge lenth of names  and O(m)is the time complexity for each value  and n is the number of elements in the dictionary (d)
+    for value in d.values():
+        if user.lower() == value.split(',')[0].lower():# split items evey , and get user which is already at index 0
+            return True
+    return False
+def find_id(id,d):#O(n*m) m is considered to be the length of each id where O(m)is the time complexity of the comparison in the keys and n is the number of elements in the dictionary 
+    for value in d.keys():
+        if id.lower() == value.split(',')[0].lower():
+            return True
+    return False
+# display fucntions for admin menu
+def genders(d):#1 O(N) n is the number of elements in the dictionary depends on whats the user enters 
     males=0
     females=0
     for value in d.values():
-      if value.split(',')[2].lower()=='male':
+      if value.split(',')[2].lower()=='male': # after splitingat , the genders index will be at 2
           males+=1
       elif value.split(',')[2].lower()=='female':
           females+=1
     
     return f'the number of females are {females} and the number of males {males}'
-
-def find_user(user, d):
-    for value in d.values():
-        if user.lower() == value.split(',')[0].lower():
-            return True
-    return False
-
-
 #https://www.tutorialspoint.com/How-to-get-formatted-date-and-time-in-Python#:~:text=To%20convert%20a%20datetime%20object,hh%3Amm%3Ass%20format.
 import datetime
-
-def add_emp():#2
+def add_emp():#2 O(k+l) since there are 2 for loops next to each other
     username = input('Enter username: ')
     while True:
         gender = input('Enter gender (male/female): ')
@@ -44,38 +44,82 @@ def add_emp():#2
             break
         else:
             print("Invalid salary. Please enter a valid number.")
-    dx = datetime.datetime.now().strftime('%Y%m%d')
+    dx = datetime.datetime.now().strftime('%Y%m%d') # the link source is next to the function from where i got it 
 
     with open('Data.txt', 'r') as lines:
-        count = len(lines.readlines()) + 1
+        count = len(lines.readlines()) + 1 # i counted the lines and since its a new employee so its an extra line 
     employee_id = f'emp00{count}'
     new_line = f'{employee_id},{username},{dx},{gender},{salary}'
     with open('Data.txt', 'a') as file:
-        file.write('\n' + new_line)
+        file.write('\n'+new_line) # writing to the text file on a new line the new employee data
     print("New employee has been added")
 
-def display_data():#3
+def display_data():#3 O(1) DISPLAYING 1 output 
     with open('Data.txt','rt') as file:
         lines=  file.readlines()
         for line in lines:
             print(line.strip('\n'))# removes the \n  spaces between the text files lines that are being printed  
-def change_slary():#4
+
+def update_salary(d):#4 O(N) diplayed according to the users input 
     while True:
-        emloyee_name=input('enter employee id')
-        if emloyee_name.lower():
-            pass
-def del_emp():#https://www.csnewbs.com/python-10c-remove-edit-lines#:~:text=Deleting%20Lines%20in%20a%20File&text=open%20the%20file%20in%20read,to%20input%20the%20exact%20line&text=Then%20open%20the%20file%20in,file%20isn't%20equal%20to&text=The%20line.
+        emp_id = input('Enter employee id: ')
+        if find_id(emp_id, d):
+            salary = float(input('Enter new slary: '))
+            with open('Data.txt', 'r') as file:
+                lines = file.readlines()
+            with open('Data.txt', 'w') as file:
+                for line in lines:
+                    if emp_id in line:
+                        data = line.split(',') # split lines on each , 
+                        new_salary = int(salary) 
+                        str_salary = str(new_salary) 
+                        data[4] = str_salary# when the data was splited its shjoe that the salary is on index 4 so we add the new salary on index 4
+                        new_line = ','.join(data) # added the new data to the line 
+                        file.write(new_line + '\n')
+                        print(f'Salary has been updated  to {data[4]}')
+                    else:
+                        file.write(line)
+            break
+        else:
+            print('Employee not found!')
+
+def del_emp():#5# O(N) also depends on the users input #https://www.csnewbs.com/python-10c-remove-edit-lines#:~:text=Deleting%20Lines%20in%20a%20File&text=open%20the%20file%20in%20read,to%20input%20the%20exact%20line&text=Then%20open%20the%20file%20in,file%20isn't%20equal%20to&text=The%20line.
   emp_id=input('enter employee id: ')
+  found=False
   with open('Data.txt','r') as file:
       lines=file.readlines()
   with open('Data.txt','w') as file:
       for line in lines:
-          if emp_id in line:
-              print('employee has been delted')
+          if emp_id in line:  
+              print('employee has been deleted')
           else:
               file.write(line)
+  if not found:
+      print('employee not found') # improvised the code a little bit according to whats asked for 
 
-def admin(d):
+def raise_salary(d):
+    while True:
+        emp_id = input('Enter employee id: ')
+        if find_id(emp_id, d):
+            rase = float(input('Enter raise: ')) 
+            with open('Data.txt', 'r') as file:
+                lines = file.readlines()
+            with open('Data.txt', 'w') as file:
+                for line in lines:
+                    if emp_id in line:
+                        data = line.split(',') # splited data on each ,
+                        new_salary = int(int(data[4]) * (rase/100)+int(data[4])) # since the slary in any line is on index 4 so we calculate it
+                        str_salary = str(new_salary) # cast to string to re enter in the text file as string
+                        data[4] = str_salary # the new raised salary is added to index 4
+                        new_line = ','.join(data) # joinded to the new line
+                        file.write(new_line + '\n') # write the new line 
+                        print(f'Salary has been raised to {data[4]}')
+                    else:
+                        file.write(line)
+            break
+        else:
+            print('Employee not found!')
+def admin(d):#O(N) depends on the users input 
   while True:
       print('1.Display Statistics')
       print('2.Add an Employee')
@@ -85,44 +129,46 @@ def admin(d):
       print('6.Raise Employees Salary')
       print('7.Exit')
       x=input('enter a number:')
-      if x.isdigit():
-        if x==1:
-          print(genders(d))
       
-        elif x==2:
+      if x=='1':
+          print(genders(d))
+      elif x=='2':
           add_emp()
-        elif x==3:
+      elif x=='3':
               display_data()
-        elif x==4:
-            pass
-        elif x==5:
+      elif x=='4':
+            update_salary(d)
+      elif x=='5':
             del_emp()
-        elif x==7:
+      elif x=='6':
+          raise_salary(d)
+      elif x=='7':
             exit()  
       else:
           print('incorrect input')  
-
-def get_salary(d, user):
-    for id, info in d.items():
-        name = info.split(',')[0]
-        if name == user:
-            salary = info.split(',')[3]
+      
+# users menu
+def get_salary(d, user):#O(n*m) which n represents the the items in the ictionary and m is the name of the salary
+    for id,info in d.items(): 
+        name = info.split(',')[0]# spliting items after each , which collects th names that  are located at index 0
+        if name == user: 
+            salary = info.split(',')[3]  # after finding if there is a user in the name  collect the slary in the data which is at index 3 
             print(f"Your salary is {salary}")
-            return  # Exit the loop once the salary is found
-def user(d, user_name):
+            return # exit loop when done 
+def user(d, user_name):#O(N) depends ofn the users input
     while True:
         print('1. Check my Salary')
         print('2. Exit')
         x = input('Enter a number: ')
-        if x.isdigit():
-          if x == 1:
+        
+        if x == '1':
              get_salary(d, user_name)
-          elif x == 2:
+        elif x == '2':
               exit()
         else:
             print("Incorrect input or number.")
 i = 0
-while i < 5:
+while i < 5: #O(1) constatnt loop repeating th same output
     user_input = input('Enter username: ')
     pws = input('Enter password: ')
     if user_input.lower() == 'admin' and pws.lower() == 'admin123123':
@@ -136,40 +182,3 @@ while i < 5:
     else:
         i += 1
         print('Wrong username or password')
-
-
-
-
-
-
-
-
-
-# file=open('Data.txt','r')
-# lines=file.readlines()
-# file.close()
-# ali='emp003'
-# file=open('Data.txt','w')
-# for line in lines:
-#     if ali in line:
-#       salary= ali.split(',')[4]*1.5
-#     else:
-#         file.write(line)
-# file.close()
-
-# ali = 'emp003'
-
-# with open('Data.txt', 'r') as file:
-#     lines = file.readlines()
-
-# with open('Data.txt', 'w') as file:
-#     for line in lines:
-#         if ali in line:
-#             parts = line.split(',')
-#             new_salary = str(int(parts[4]) * 1.5)  # Multiply the salary by 1.5 and convert to string
-#             parts[4] = new_salary
-#             new_line = ','.join(parts)
-#             file.write(new_line)
-#         else:
-#             file.write(line)
-
